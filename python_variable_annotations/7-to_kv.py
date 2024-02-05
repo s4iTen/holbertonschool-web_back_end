eb71b784f476240ce800aabd897631a24f8d33aa
s4iTen <ychouk102@gmail.com>
2024-02-05 17:22:36 +0100

    this function return a tuple that contain string and float

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+this function return a tuple that contain string and float
+"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    this is the return of tuple
+    """
+    return (k, v)
