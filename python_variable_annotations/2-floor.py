6ad80e71c8725a5c3a10488c44a196e822444bad
s4iTen <ychouk102@gmail.com>
2024-02-05 16:17:30 +0100

    this function takes a float and return the floor of it

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+this function takes a float and return the floor of it
+"""
+
+
+def floor(n: float) -> int:
+    """
+    n -> float
+    return: return the floor of the float
+    """
+    return int(n)
