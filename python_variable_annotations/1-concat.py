c482325527e4dd573b564351dfbed637f8d1a705
s4iTen <ychouk102@gmail.com>
2024-02-05 16:09:22 +0100

    this is the concat function

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+concatinate two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    str1 -> string
+    str2 -> string
+
+    return: return the concatination of the two strings
+    """
+    return str1 + str2
