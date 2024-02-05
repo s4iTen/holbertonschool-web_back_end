a37ee3aea6a3c8294afbe1a82b3ef5b686f32ca6
s4iTen <ychouk102@gmail.com>
2024-02-05 16:21:46 +0100

    this function take a float and return it as a string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+takes the float and return it as string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    n -> float
+    return: return the str of the float
+    """
+    return str(n)
