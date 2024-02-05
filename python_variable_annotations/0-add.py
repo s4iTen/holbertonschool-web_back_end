11470212d8eb3006ce32e8eb14637ac4da333133
s4iTen <ychouk102@gmail.com>
2024-02-05 15:38:15 +0100

    this is the last updates

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-""" this is the add function """
-def add(a: float, b: float) -> float:
+"""this is the add function
+"""
 
+
+def add(a: float, b: float) -> float:
     return a + b
