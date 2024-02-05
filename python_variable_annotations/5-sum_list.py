bfef604f16b5ac4ec4d852b4b1ac8f1aa4a053d6
s4iTen <ychouk102@gmail.com>
2024-02-05 16:45:09 +0100

    this function return the sum of the float inside the list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import typing
+"""
+this function add each float in the list
+"""
+
+
+def sum_list(input_list: typing.List[float]) -> float:
+    """
+    input_list -> list with float elements inside
+    return: the sum of the float inside the list
+    """
+    temp = 0
+    for i in range(len(input_list)):
+        temp += input_list[i]
+    return float(temp)
