4cbe8841bca8c008d97c3b05ca17d40baeb63787
s4iTen <ychouk102@gmail.com>
2024-02-06 15:14:42 +0100

    this is the last updates

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -3,20 +3,18 @@
 this function return the average time per task
 """
 import time
-from typing import List
-from asyncio import run, gather
-from random import uniform
+from asyncio import gather
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """
     n -> integer
     max_delay -> integer
     return: float
     """
     start = time.time()
-    await gather(wait_n(n, max_delay))
+    gather(wait_n(n, max_delay))
     end = time.time()
     total = end - start
     measure = total / n
