9a353dc64e91a61c42c0455275dd0e3b46735e6a
s4iTen <ychouk102@gmail.com>
2024-02-07 13:57:59 +0100

    this coroutine just yield a random float and sleep 1 sec for each num

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+this function just yield random float between 0 and 10
+"""
+import random
+import asyncio
+
+
+async def async_generator():
+    """
+    there is no return or parameters
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield (random.uniform(0, 10))
