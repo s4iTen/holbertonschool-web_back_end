2f752f1881f0cfa9e0d34c63d7f5b18c9a21159a
s4iTen <ychouk102@gmail.com>
2024-02-07 16:14:02 +0100

    this is the last updates

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -9,6 +9,8 @@ import asyncio
 async def async_generator():
     """
     there is no return or parameters
+
+    float: A random float value between 0 and 10.
     """
     for i in range(10):
         await asyncio.sleep(1)
