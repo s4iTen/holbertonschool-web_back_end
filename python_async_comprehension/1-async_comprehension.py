33e4522a7d84b528564bcc1140a4dfa811f74b85
s4iTen <ychouk102@gmail.com>
2024-02-08 14:04:10 +0100

    this function collect 10 num and return them

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+this coroutine collect 10 num and return them
+"""
+import typing
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> typing.List:
+    """
+    this coroutine collect 10 num and return them
+    """
+    result = []
+    async for i in async_generator():
+        result.append(i)
+    return result
