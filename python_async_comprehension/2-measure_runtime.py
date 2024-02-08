162bce0fae369f5748fa815090ade918be381b82
s4iTen <ychouk102@gmail.com>
2024-02-08 14:23:57 +0100

    this coroutine measure the time of the execution of the previous coroutine four time

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
-""""""
+"""
+this coroutine measure the time of the execution of the previous coroutine four time"""
 import time
-import typing
 from asyncio import gather
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
+    """
+    this coroutine measure the time of the execution of the previous coroutine four time
+    """
     start = time.time()
     await gather(async_comprehension(),
                  async_comprehension(),
@@ -15,4 +18,3 @@ async def measure_runtime() -> float:
 
     end = time.time()
     return end - start
-    
