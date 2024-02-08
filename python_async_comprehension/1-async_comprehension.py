b0f8043a507000ba0fb14708495de5ca9b4c6ad2
s4iTen <ychouk102@gmail.com>
2024-02-08 14:08:46 +0100

    this is the last updates

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -5,12 +5,8 @@ this coroutine collect 10 num and return them
 import typing
 async_generator = __import__('0-async_generator').async_generator
 
-
 async def async_comprehension() -> typing.List[float]:
     """
     this coroutine collect 10 num and return them
     """
-    result = []
-    async for i in async_generator():
-        result.append(i)
-    return result
+    return [i async for i in async_generator()]
