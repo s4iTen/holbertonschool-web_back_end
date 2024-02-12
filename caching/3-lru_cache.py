4e55945481e48685436857d5a59b50853e3cb488
s4iTen <ychouk102@gmail.com>
2024-02-12 13:29:37 +0100

    this is the lru method class

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -1,5 +1,10 @@
+#!/usr/bin/python3
+"""
+this is the main class
+"""
 from collections import OrderedDict
 BaseCaching = __import__('base_caching').BaseCaching
+
 
 class LRUCache(BaseCaching):
     '''
