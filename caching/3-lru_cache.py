016fbb5f7d8090eaa19ef091492cc90ecafae33b
s4iTen <ychouk102@gmail.com>
2024-02-12 13:28:20 +0100

    this is the lru method class

---
+++ caching/3-lru_cache.py
@@ -0,0 +1,29 @@
+from collections import OrderedDict
+BaseCaching = __import__('base_caching').BaseCaching
+
+class LRUCache(BaseCaching):
+    '''
+    __init__(): Initializes the LRUCache object.
+    put: Adds an item to the cache.
+    get: Retrieves an item from the cache based on the key.
+    '''
+
+    def __init__(self):
+        super().__init__()
+        self.access_order = OrderedDict()
+
+    def put(self, key, item):
+        if key is not None and item is not None:
+            # Check if the cache is full
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                # Discard the least recently used item (LRU)
+                lru_key = next(iter(self.access_order))
+                del self.access_order[lru_key]
+                del self.cache_data[lru_key]
+                print(f"DISCARD: {lru_key}")
+            self.cache_data[key] = item
+            self.access_order[key] = None
+
+    def get(self, key):
+        """Get an item by key"""
+        return self.cache_data.get(key) if key is not None else None
