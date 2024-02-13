aac07ca8b52ded84c44764bbd7220d69ab7ebb19
s4iTen <ychouk102@gmail.com>
2024-02-13 14:55:05 +0100

    this is the last updates

--- caching/4-mru_cache.py
+++ caching/4-mru_cache.py
@@ -1,46 +1,27 @@
 #!/usr/bin/python3
-"""
-this is the main class
-"""
+"""MRU Caching"""
 from collections import OrderedDict
-BaseCaching = __import__('base_caching').BaseCaching
+from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    '''
-    LRUCache class
-    __init__(): Initializes the LRUCache object.
-    put: Adds an item to the cache.
-    get: Retrieves an item from the cache based on the key.
-    '''
+    """MRUCache that inherits from
+    BaseCaching and is a caching system:"""
 
     def __init__(self):
-        """
-        this is the initialization method
-        """
         super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """
-        this must assign to the dictionary
-        """
-        if key is None or item is None:
-            return
-
-        self.cache_data[key] = item
-
-        # Check if the number of items exceeds the maximum allowed
-        if len(self.cache_data) > self.MAX_ITEMS:
-            # Find and discard the most recently used item
-            mru_key = self.get_mru_key()
-            del self.cache_data[mru_key]
-            print(f"DISCARD: {mru_key}\n")
-
-    def get_mru_key(self):
-        """
-        this is the get mru key method
-        """
-        return next(reversed(self.cache_data))
+        """Must assign to the dictionary """
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discarded_key = next(iter(self.cache_data.keys()))
+                print(f"DISCARD: {discarded_key}")
+                self.cache_data.popitem(last=False)
+            self.cache_data[key] = item
 
     def get(self, key):
         """Get an item by key"""
