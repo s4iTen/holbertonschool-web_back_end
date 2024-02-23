fdc33e3c27ca8295e7fc8c39f348e92b8467d24b
s4iTen <ychouk102@gmail.com>
2024-02-23 21:41:40 +0100

    this is the basic auth changement

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -8,17 +8,18 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
-
+from api.v1.auth.basic_auth import BasicAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
-auth_type = getenv("AUTH_TYPE")
-if auth_type == "auth":
+AUTH_TYPE = getenv("AUTH_TYPE")
+if AUTH_TYPE == "basic_auth":
+    auth = BasicAuth()
+else:
     auth = Auth()
-
 
 @app.errorhandler(404)
 def not_found(error) -> str:
