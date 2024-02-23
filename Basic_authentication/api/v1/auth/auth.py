df2423a93930fe3cd671e7a931c9c0e6f033ce8c
s4iTen <ychouk102@gmail.com>
2024-02-23 16:52:52 +0100

    this is the Authorization directory

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+""" Auth class """
 from flask import request
 from typing import List, TypeVar
 
 
 class Auth():
+    """ Auth class"""
 
     def __init__(self):
         return None
