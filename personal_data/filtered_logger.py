8b8be2bafcf0541dbbe6c3db204e4625e39bd364
s4iTen <ychouk102@gmail.com>
2024-02-23 13:15:04 +0100

    this is the last updates

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -31,15 +31,19 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """ init """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """ format method """
         return filter_datum(
             self.fields,
             self.REDACTION,
             super().format(record),
-            self.SEPARATOR)
+            self.SEPARATOR
+        )
+
 
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
