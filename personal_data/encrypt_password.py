c9038755e634ea7cbdbe85496f42a04616cba4b1
s4iTen <ychouk102@gmail.com>
2024-02-23 14:02:03 +0100

    this is the last updates

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ Hashing Password """
 
-
 import bcrypt
+
 
 def hash_password(password: str) -> bytes:
     """ Hashing Password Function """
@@ -10,3 +10,9 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(_bytes, salt)
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Check Password """
+    _bytes = password.encode('utf-8')
+    return bcrypt.checkpw(_bytes, hashed_password)
