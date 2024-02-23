d8ffa653aa90c5662612d16aeee750492bee9217
s4iTen <ychouk102@gmail.com>
2024-02-23 21:46:38 +0100

    this is the last updates of the basic auth class

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,3 +7,12 @@ class BasicAuth(Auth):
     """ basicAuth class """
     def __init__(self):
         return
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ extract_base64_authorization_header method """
+        if authorization_header is None or type(authorization_header) != str:
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]
