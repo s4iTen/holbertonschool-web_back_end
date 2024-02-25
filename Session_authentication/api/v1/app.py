a6c8791f3653d4c83eb5261728ac1ea7ef76db62
s4iTen <ychouk102@gmail.com>
2024-02-25 15:02:51 +0100

    this is the session auth class and the changement of the app

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -8,6 +8,7 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -17,6 +18,8 @@ auth = None
 AUTH_TYPE = getenv("AUTH_TYPE")
 if AUTH_TYPE == "basic_auth":
     auth = BasicAuth()
+elif AUTH_TYPE == "session_auth":
+    auth = SessionAuth()
 else:
     auth = Auth()
 
---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+from api.v1.auth.auth import Auth
+
+class SessionAuth(Auth):
+    pass
