df13f34594630d527eef58ab7a52c4ee51859c3b
s4iTen <ychouk102@gmail.com>
2024-02-26 16:40:04 +0100

    this is the last updates

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -31,9 +31,7 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """
-        Add a new user to the database
-        """
+        """ Add a new user to the database """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
