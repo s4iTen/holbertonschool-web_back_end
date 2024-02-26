848072b41bcebfb77013308cd61ce27a6b9f41ef
s4iTen <ychouk102@gmail.com>
2024-02-26 15:17:52 +0100

    this is the last updates

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -9,7 +9,7 @@ class User():
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String(255), unique=True)
+    email = Column(String(255), unique=True, nullable=False)
     hashed_password = Column(String(255), nullable=False)
     session_id = Column(String(255), nullable=False)
     reset_token = Column(String(255), nullable=False)
