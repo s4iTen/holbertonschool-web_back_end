07b4d7fd46365caa914b4899da5ba701171a7894
s4iTen <ychouk102@gmail.com>
2024-02-26 15:57:20 +0100

    this is the last updates

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -4,9 +4,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from user import User
 
 from user import Base
+from user import User
 
 
 class DB:
