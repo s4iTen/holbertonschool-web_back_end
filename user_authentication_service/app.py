5e6168bec076be089a50f526e04532464816a2d5
s4iTen <ychouk102@gmail.com>
2024-02-28 14:29:42 +0100

    this is the delete function and the get profile function

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -42,5 +42,26 @@ def login():
     return response
 
 
+@app.route("/sessions", methods=["DELETE"])
+def logout():
+    """this is the logout function"""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route("/profile", methods=["GET"])
+def profile():
+    """this is the profile function"""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    return jsonify({"email": user.email}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
