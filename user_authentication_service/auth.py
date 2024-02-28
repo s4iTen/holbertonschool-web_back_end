df94b1f346ceb5bd9ca42638529f89c5b70aa0fe
s4iTen <ychouk102@gmail.com>
2024-02-28 13:00:21 +0100

    this is the class auth and the method register that check if the user is already exist and if not it create a new user

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 ''' Hash password '''
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -11,3 +14,26 @@ def _hash_password(password: str) -> bytes:
     passwd = password.encode('utf-8')
     crypt_pass = bcrypt.gensalt()
     return bcrypt.hashpw(passwd, crypt_pass)
+
+
+class Auth:
+    """
+    Auth class to interact with the authentication database.
+    """
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        this function takes argument email as string
+        and password as string
+        and returns boolean
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError(f"User {email} already exists.")
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+            return user
