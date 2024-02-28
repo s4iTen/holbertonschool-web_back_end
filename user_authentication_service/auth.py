8b507b4765544a4593a2696f12b13759c38b1dbd
s4iTen <ychouk102@gmail.com>
2024-02-28 13:47:25 +0100

    this is the create session function that generate uuid and store it in the data base as session id and return it

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -60,3 +60,17 @@ class Auth:
                                       user.hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """
+        this function takes argument email as string
+        and returns a string
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                session_id = _generate_uuid()
+                self._db.update_user(user.id, session_id=session_id)
+                return session_id
+        except NoResultFound:
+            return None
