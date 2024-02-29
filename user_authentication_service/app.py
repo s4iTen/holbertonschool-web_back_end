1096bbc3bb96f1aad941abf750e382a0eecb873b
s4iTen <ychouk102@gmail.com>
2024-02-29 14:42:38 +0100

    this is the last updates

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -86,7 +86,7 @@ def update_password():
     try:
         AUTH.update_password(reset_token, new_password)
         return jsonify({"email": email, "message": "Password updated"}), 200
-    except ValueError:
+    except NoResultFound:
         abort(403)
 
 
