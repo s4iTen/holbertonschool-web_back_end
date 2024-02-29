13ede0d936b5d65640d48302d2781bac41233f86
s4iTen <ychouk102@gmail.com>
2024-02-29 14:41:16 +0100

    this is the last updates

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -84,7 +84,7 @@ def update_password():
     reset_token = request.form.get("reset_token")
     new_password = request.form.get("new_password")
     try:
-        AUTH.update_password(reset_token, new_password)
+        AUTH.update_password(email, reset_token, new_password)
         return jsonify({"email": email, "message": "Password updated"}), 200
     except ValueError:
         abort(403)
