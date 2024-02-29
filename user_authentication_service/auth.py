b9cc4989dbc3d8da80b6e544d65f5d9e0ac21e9f
s4iTen <ychouk102@gmail.com>
2024-02-29 14:15:24 +0100

    this method updates the user token if its exist and return it as string

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -111,6 +111,5 @@ class Auth:
                 token = _generate_uuid()
                 self._db.update_user(user.id, reset_token=token)
                 return token
-        except ValueError :
+        except ValueError:
             raise ValueError
-
