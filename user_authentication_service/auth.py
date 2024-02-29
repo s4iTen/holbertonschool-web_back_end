4524f93d4fc4f505d3e8dca50d0a5e15bd21efcd
s4iTen <ychouk102@gmail.com>
2024-02-29 14:24:30 +0100

    This is the update_password Menthod

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -113,3 +113,15 @@ class Auth:
                 return token
         except NoResultFound:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """ This is the update_password Menthod """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            if user:
+                hashed_password = _hash_password(password)
+                self._db.update_user(user.id, hashed_password=hashed_password,
+                                    reset_token=None)
+            return None
+        except NoResultFound:
+            raise ValueError
