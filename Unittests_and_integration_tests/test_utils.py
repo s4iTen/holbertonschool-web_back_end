03f5e5c9fecc36db2364888bb6465b27b08c9bff
s4iTen <ychouk102@gmail.com>
2024-03-05 20:27:32 +0100

    this is the Test case for the memoize decorator

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,7 @@
 """get_json function"""
 import unittest
 from unittest.mock import Mock, patch
-from utils import get_json
+from utils import get_json, memoize
 from parameterized import parameterized
 
 
@@ -21,6 +21,31 @@ class TestGetJson(unittest.TestCase):
             real_response = get_json(test_url)
             self.assertEqual(real_response, test_payload)
             mock_response.json.assert_called_once()
+
+
+class TestMemoize(unittest.TestCase):
+    """Test case for the memoize decorator"""
+
+    class TestClass:
+        """Test class with memoized method"""
+
+        def a_method(self):
+            return 42
+
+        @memoize
+        def a_property(self):
+            return self.a_method()
+
+    def test_memoize(self):
+        """Test memoization of a_property"""
+        test_instance = self.TestClass()
+        with patch.object(test_instance, 'a_method') as mock_a_method:
+            mock_a_method.return_value = 42
+            result1 = test_instance.a_property()
+            result2 = test_instance.a_property()
+            mock_a_method.assert_called_once()
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
 
 
 if __name__ == '__main__':
