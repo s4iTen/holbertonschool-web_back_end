d3f84c8af930bd2a2561fb7b1504c62ce55f5fa9
s4iTen <ychouk102@gmail.com>
2024-03-05 20:29:13 +0100

    this is the last updates

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -6,23 +6,6 @@ from utils import get_json, memoize
 from parameterized import parameterized
 
 
-class TestGetJson(unittest.TestCase):
-    """Test cases for the get_json function"""
-
-    @parameterized.expand([
-        ("http://example.com", {"payload": True}),
-        ("http://holberton.io", {"payload": False})
-    ])
-    def test_get_json(self, test_url, test_payload):
-        """Test Get JSON Method"""
-        mock_response = Mock()
-        mock_response.json.return_value = test_payload
-        with patch('requests.get', return_value=mock_response):
-            real_response = get_json(test_url)
-            self.assertEqual(real_response, test_payload)
-            mock_response.json.assert_called_once()
-
-
 class TestMemoize(unittest.TestCase):
     """Test case for the memoize decorator"""
 
