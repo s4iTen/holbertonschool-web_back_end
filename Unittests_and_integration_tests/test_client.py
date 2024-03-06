4edccb8c4be402d37f28fba6eb26571c60b07c29
s4iTen <ychouk102@gmail.com>
2024-03-06 15:00:19 +0100

    this is the last updates

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -38,6 +38,7 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch("client.get_json", return_value=[{"name": "holberton"}])
     def test_public_repos(self, mock_get_json):
         """Test Public Repos Method"""
+
         with patch.object(GithubOrgClient, '_public_repos_url',
                           new_callable=PropertyMock,
                           return_value='https://api.github.com/'
