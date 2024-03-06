34da75fde24ab2ee9c1615b8f8d91203bbb0b8fa
s4iTen <ychouk102@gmail.com>
2024-03-06 09:37:22 +0100

    this is the Test Public Repos Url Method

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """Test Client File"""
 import unittest
 from parameterized import parameterized
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from client import GithubOrgClient
 
 
@@ -20,6 +20,20 @@ class TestGithubOrgClient(unittest.TestCase):
         client.org()
         mock_json.assert_called_once_with(
             f"https://api.github.com/orgs/{test_org}")
+
+    def test_public_repos_url(self):
+        """Test Public Repos Url Method"""
+        with patch.object(GithubOrgClient, 'org',
+                          new_callable=PropertyMock,
+                          return_value={'repos_url': 'holberton'}
+                          ) as mock_org:
+            test_json = {"repos_url": 'holberton'}
+            test_client = GithubOrgClient(test_json.get("repos_url"))
+            test_return = test_client._public_repos_url
+            mock_org.assert_called_once
+            self.assertEqual(
+                test_return,
+                mock_org.return_value.get("repos_url"))
 
 
 if __name__ == "__main__":
