45ae94abb3ec57ed3990f076be16d0763ff93ba3
s4iTen <ychouk102@gmail.com>
2024-03-08 12:59:13 +0100

    this is the last updates

--- i18n/2.app.py
+++ i18n/2.app.py
@@ -7,19 +7,26 @@ app = Flask(__name__)
 babel = Babel(app)
 
 
-@babel.localeselector
-def get_locale():
-    """get locale"""
-    user = getattr(g, 'user', None)
-    if user is not None:
-        return user.get('locale')
-    return request.accept_languages.best_match(['en', 'fr'])
+class Config(object):
+    """ Class will configure available languages in the app """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
 
 
 @app.route('/', strict_slashes=False)
 def index():
     """index route"""
     return render_template('2-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """get locale"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == '__main__':
