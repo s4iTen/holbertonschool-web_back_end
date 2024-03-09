99c153fab61b19bf7c0bcd2751287a9678f4a711
s4iTen <ychouk102@gmail.com>
2024-03-09 16:16:47 +0100

    this is the last updates

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -7,7 +7,6 @@ from flask_babel import Babel, gettext
 app = Flask(__name__)
 babel = Babel(app)
 gettext.__doc__ = "Documentation for gettext"
-'''this is the documentation'''
 
 
 class Config(object):
