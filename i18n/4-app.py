6cf018995dab966eecea975d1bbc1902f111749a
s4iTen <ychouk102@gmail.com>
2024-03-09 16:54:47 +0100

    this is the last updates

---
+++ i18n/4-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" Task 3: Parametrize templates """
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """ Class will configure available languages in the app """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """ Returning our html page """
+    return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """ Getting locale from request.accept_languages """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+def gettext(text):
+    """Translate text to the currently selected locale."""
+    return text
