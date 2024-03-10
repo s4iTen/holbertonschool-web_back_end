c6e76697c0923723c1e09c8ec75f34ab84c58fc9
s4iTen <ychouk102@gmail.com>
2024-03-10 17:51:34 +0100

    this is the last updates

--- i18n/6-app.py
+++ i18n/6-app.py
@@ -7,10 +7,8 @@ from flask_babel import Babel, gettext
 app = Flask(__name__)
 babel = Babel(app)
 gettext.__doc__ = "Documentation for gettext"
-""" Checker requirements """
 
 
-""" Emulate user login system with user table """
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
@@ -20,7 +18,6 @@ users = {
 
 
 class Config(object):
-    """ Class will configure available languages in the app """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
@@ -31,40 +28,36 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    """ Returning our html page """
     return render_template('5-index.html')
 
 
 @babel.localeselector
 def get_locale():
-    """ Getting locale from request.accept_languages """
-    locale = request.args.get("locale")
-    if locale is not None and locale in Config.LANGUAGES:
-        return locale
-    try:
-        user = get_user()
-        if user and user['locale'] in Config.LANGUAGES:
-            return user['locale']
-        raise Exception
-    except Exception:
-        return request.accept_languages.best_match(app.config['LANGUAGES'])
+    locale_from_url = request.args.get("locale")
+    if locale_from_url is not None and locale_from_url in Config.LANGUAGES:
+        return locale_from_url
+    
+    user = g.get('user')
+    if user and user['locale'] in Config.LANGUAGES:
+        return user['locale']
+    
+    locale_from_header = request.accept_languages.best_match(app.config['LANGUAGES'])
+    if locale_from_header is not None:
+        return locale_from_header
+    
+    return app.config['BABEL_DEFAULT_LOCALE']
 
 
 def get_user():
-    """ returns a user dictionary or None if the ID cannot be
-    found or if login_as was not passed """
     try:
-        user_Id = request.args.get('login_as')
-        return users[int(user_Id)]
-    except Exception:
+        user_id = request.args.get('login_as')
+        return users[int(user_id)]
+    except (ValueError, TypeError, KeyError):
         return None
 
 
 @app.before_request
 def before_request():
-    """ Decorator to make it be executed before all other functions.
-    Use get_user to find a user if any, and set it as a global on flask.g.user.
-    """
     g.user = get_user()
 
 
