12e288b068364062164caed5d58deba508b9690f
s4iTen <ychouk102@gmail.com>
2024-03-18 09:23:31 +0100

    this is the get method and the convert of the data to the original type what ever it be str or int

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,7 +2,7 @@
 '''this is the redis exercise'''
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache():
@@ -18,3 +18,26 @@ class Cache():
         self._redis.set(random_key, data)
 
         return random_key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """ get method and recovering original type"""
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+
+        return value
+    
+    def get_str(self, key: str) -> str:
+        """ Parameterize the value from redis to str """
+        value = self._redis.get(key)
+        return value.decode("utf-8")
+    
+    def get_int(self, key: str) -> int:
+        """ Parameterize the value from redis to int """
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode("utf-8"))
+        except Exception:
+            value = 0
+        return value
