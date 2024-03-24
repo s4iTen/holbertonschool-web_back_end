b341a4e1a366f36e06dc0044a51ed2d65403b808
s4iTen <ychouk102@gmail.com>
2024-03-24 15:54:02 +0100

    this is the last updates

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,6 +1,4 @@
 -- SQL script that creates a stored procedure ComputeAverageScoreForUser 
--- that computes and store the average score for a student. 
--- Note: An average score can be a decimal
 
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER |
