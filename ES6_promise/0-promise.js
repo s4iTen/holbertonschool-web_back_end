39625f779e22e5fc3d0f001c8012a34d8a1f0c08
s4iTen <ychouk102@gmail.com>
2024-03-24 17:09:53 +0100

    this is the getResponseFromAPI function

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+export default function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+      const condition = true;
+      if (condition) resolve('true');
+      else reject(new Error('false error message'));
+    });
+  }
