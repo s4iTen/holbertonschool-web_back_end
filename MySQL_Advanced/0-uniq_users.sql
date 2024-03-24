4de07a4674bd8f1ca7ada582e424a032ca540fdf
s4iTen <ychouk102@gmail.com>
2024-03-24 14:44:30 +0100

    this is the last updates

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
--- this is the creation of the table
+--  this is the creation of the table
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255) NOT NULL
+    name VARCHAR(255)
 );
