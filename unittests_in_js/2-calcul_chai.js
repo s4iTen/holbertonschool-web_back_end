18b0c7b33e40b80f8e31a81191b92315c244cd58
s4iTen <ychouk102@gmail.com>
2024-04-29 03:21:34 +0200

    this is the last updates

--- unittests_in_js/2-calcul_chai.js
+++ unittests_in_js/2-calcul_chai.js
@@ -1,5 +1,5 @@
 // 1-calcul.js
-export const calculateNumber = (type, a, b) => {
+const calculateNumber = (type, a, b) => {
     const roundedA = Math.round(a);
     const roundedB = Math.round(b);
 
@@ -9,8 +9,10 @@ export const calculateNumber = (type, a, b) => {
         case 'SUBTRACT':
             return roundedA - roundedB;
         case 'DIVIDE':
-            return roundedB === 0 || roundedA === 0 ? 'Error' : roundedA / roundedB;
+            return roundedB === 0 ? 'Error' : roundedA / roundedB;
         default:
             throw new Error('Invalid type. Use SUM, SUBTRACT, or DIVIDE.');
     }
 };
+
+module.exports = calculateNumber;
