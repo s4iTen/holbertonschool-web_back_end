31791eca8df91322709dcd3b601987359e8f74c4
s4iTen <ychouk102@gmail.com>
2024-04-29 03:24:14 +0200

    this is the last updates

---
+++ unittests_in_js/utils.js
@@ -0,0 +1,19 @@
+const Utils = {
+    calculateNumber(type, a, b) {
+        const roundedA = Math.round(a);
+        const roundedB = Math.round(b);
+
+        switch (type) {
+            case 'SUM':
+                return roundedA + roundedB;
+            case 'SUBTRACT':
+                return roundedA - roundedB;
+            case 'DIVIDE':
+                return roundedB === 0 ? 'Error' : roundedA / roundedB;
+            default:
+                throw new Error('Invalid type. Use SUM, SUBTRACT, or DIVIDE.');
+        }
+    }
+};
+
+module.exports = Utils;
