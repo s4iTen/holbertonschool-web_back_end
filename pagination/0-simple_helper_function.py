e6194f7921ac7746368a160d2b8dad83a5cc3d9d
s4iTen <ychouk102@gmail.com>
2025-01-29 15:30:10 +0100

    this is the last updates

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -4,6 +4,7 @@ def index_range(page: int, page_size: int) -> tuple:
     """Return a tuple of size two containing a start index and an end index
     corresponding to the range of indexes to return in a list for those
     particular pagination parameters."""
+
     if page <= 1:
         return (0, page_size)
 
